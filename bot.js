0e0026ee98e450fabd0a48d611710f8f231a5345
Shakti <shakti.balaji712@gmail.com>
2021-12-10 23:16:42 +0530

    added 2 functions

--- bot.js
+++ bot.js
@@ -1,10 +1,38 @@
-require('dotenv').config()
-const { Client } = require('discord.js')
+import dotenv from 'dotenv'
+dotenv.config()
 
-const client = new Client()
+import Discord from 'discord.js'
+
+import { aboutData } from './data/aboutData'
+import { memberData } from './data/memberData'
+
+const client = new Discord.Client()
 
 client.on('ready', () => {
   console.log(`${client.user.tag}`)
+  
+})
+
+client.on('message', function(message){
+  if(message.author.bot)
+    return
+  const pre = "#"
+  if (!message.content.startsWith(pre))
+    return
+  const args = message.content.split(' ')
+  const command = args[0]
+  if(command==="#tellmeabout") {
+    const tellme = aboutData.find(obj => obj.name===args[1])
+    const embed = new Discord.MessageEmbed().setColor('#85850d')
+    embed.description = tellme.about
+    message.reply(embed)
+  }
+  else if(command==="#TTspeaker") {
+    const index = Math.floor(Math.random()*29);
+    const embed = new Discord.MessageEmbed().setColor('#85850d')
+    embed.description = memberData[index]
+    message.reply(embed)
+  }
 })
 
 client.login(process.env.CLIENT_TOKEN)
